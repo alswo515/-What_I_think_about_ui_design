98778fb9d2c17e0aa18d647a2baf25e8e87920e3
alswo515 <67941961+alswo515@users.noreply.github.com>
2020-11-14 18:31:31 +0900

    도마뱀 호버시 슬라이드 추가

--- js/index.js
+++ js/index.js
@@ -6,36 +6,41 @@
             $('.site_main').fadeOut(200)
         },function(){
             $('.siteMainOn canvas').fadeIn(500)
+            $('.multi_rolling').fadeIn(1500)
         }
     )
     
     $('main button').on('click',function(){
-        $('.siteMainOn canvas').fadeOut(500);
+        $('.siteMainOn canvas, .multi_rolling').fadeOut(500);
         $('.site_main').fadeIn(200);
     })
 
-    $('.slide-inner').slick({
-        autoplay: true,
-        dots: true,
-        autoplaySpeed: 4500,
-        speed: 600,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        pauseOnHover: true,
-        pauseOnDotsHover: false,
-        pauseOnFocus: false,
-        cssEase: 'ease',
-        draggable: true, //드래그 해서 화면이 바뀌냐 안바뀌냐 모바일에서는 손으로 밀어도 되는것
-        fade: false,
-        arrows: true,
-        prevArrow: '<button class="prevArrow marrow"><i class="fas fa-angle-left"></i></button>',
-        nextArrow: '<button class="nextArrow marrow"><i class="fas fa-angle-right"></i></button>',
-        responsive: [{ //자바스크립트에서 주는 반응형 디자인
-            breakpoint: 801, //801미만 사이즈
-            settings: {
-                arrows: false,
-                fade: true
-            }
+
+    $(".multi_visual").slick({
+        autoplay: true, // 자동재생
+        autoplaySpeed: 3000, // 간격시간
+        dots: false, // 동그라미버튼
+        speed: 600, // 바뀌는시간(생략가능)
+        centerMode: true,
+        centerPadding: "80px",
+        slidesToShow: 3, // 보여질슬라이드수(생략가능)
+        slidesToScroll: 1, // 이동슬라이드수(생략가능)
+        pauseOnHover: true, // 마우스오버시 멈춤여부(생략가능)
+        pauseOnDotsHover: true, // 동그라미번호버튼에 호버시 멈춤여부(생략가능)
+        pauseOnFocus: false, // 동그라미번호버튼 클릭시 자동실행 멈춤여부
+        cssEase: 'linear', // 속도함수(생략가능)
+        draggable: true, // 마우스드래그시 슬라이드 교체가능여부(생략가능)
+        fade: false, // 슬라이드가 수평으로 이동하지 않고, 제자리에서 사라지고 나타남(생략가능)
+        arrows: true, // 좌우화살표 사용여부(생략가능)
+        prevArrow: '<button class="prevArrow  marrow"><i class="fas fa-angle-left"></i></button>',
+        nextArrow: '<button class="nextArrow  marrow"><i class="fas fa-angle-right"></i></button>',
+        responsive: [{
+                breakpoint: 800,
+                settings: {
+                        centerMode: true,
+                        centerPadding: "100px",
+                        slidesToShow: 1, // 보여질슬라이드수(생략가능)
+                }
         }]
     })
 
