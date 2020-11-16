9e0e191ff46a779d58b8772136f0ca9e68b64831
alswo515 <67941961+alswo515@users.noreply.github.com>
2020-11-16 18:31:29 +0900

    슬릭슬라이더랑 카멜레온 섹션나누고 이벤트 더 빨리 나타나게하기 버튼 없애기

--- js/index.js
+++ js/index.js
@@ -3,18 +3,21 @@
 
     $('.site_main img').hover(
         function(){
-            $('.site_main').fadeOut(200)
+            $('.site_main').fadeOut(100)
+            $('main button').fadeIn(100)
         },function(){
-            $('.siteMainOn canvas').fadeIn(500)
-            $('.multi_rolling').fadeIn(1500)
+            $('main canvas').fadeIn(100)
         }
     )
     
     $('main button').on('click',function(){
-        $('.siteMainOn canvas, .multi_rolling').fadeOut(500);
-        $('.site_main').fadeIn(200);
+        $('main canvas').fadeOut(100);
+        $('.site_main').fadeIn(100)
     })
-
+    //논리값 변환시키기
+    if( $('.site_main img').fadeIn() ){
+        $('main button').fadeOut(100)
+    }
 
     $(".multi_visual").slick({
         autoplay: true, // 자동재생
@@ -25,8 +28,8 @@
         centerPadding: "80px",
         slidesToShow: 3, // 보여질슬라이드수(생략가능)
         slidesToScroll: 1, // 이동슬라이드수(생략가능)
-        pauseOnHover: true, // 마우스오버시 멈춤여부(생략가능)
-        pauseOnDotsHover: true, // 동그라미번호버튼에 호버시 멈춤여부(생략가능)
+        pauseOnHover: false, // 마우스오버시 멈춤여부(생략가능)
+        pauseOnDotsHover: false, // 동그라미번호버튼에 호버시 멈춤여부(생략가능)
         pauseOnFocus: false, // 동그라미번호버튼 클릭시 자동실행 멈춤여부
         cssEase: 'linear', // 속도함수(생략가능)
         draggable: true, // 마우스드래그시 슬라이드 교체가능여부(생략가능)
